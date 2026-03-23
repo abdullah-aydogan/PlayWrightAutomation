2d9e5258834608a8582ea683d36fb548dc8a97c8
abdullah-aydogan <abdullahaydogan245@outlook.com>
2026-03-23 08:25:27 +0300

    section 5

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -30,10 +30,50 @@ test('Browser Context-Validating Error Login', async ({browser}) => {
     console.log(allTitles);
 });
 
-test('Page Playwright Test', async ({page}) => {
+test('UI Controls', async ({page}) => {
 
-    await page.goto("https://google.com.tr/");
-    console.log(await page.title());
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
-    await expect(page).toHaveTitle("Google");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const documentLink = page.locator("[href*='documents-request']");
+    const dropdown = page.locator("select.form-control");
+
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();
+    await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
+
+test('Child windows handle', async ({browser}) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    const userName = page.locator('#username');
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const documentLink = page.locator("[href*='documents-request']");
+
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        documentLink.click()
+    ]);
+
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ")[0]
+
+    // console.log(domain);
+    await page.locator("#username").fill(domain);
+    console.log(await page.locator("#username").inputValue());
 });
